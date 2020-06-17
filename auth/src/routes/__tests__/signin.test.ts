6fee25bb98d60abc1532e8c17b11a612940e052e
nickcoleman <ncoleman@marketaxess.com>
2020-06-17 10:00:03 -0600

    signin tests - note: not finding current user

---
+++ auth/src/routes/__tests__/signin.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import {app} from '../../app';
+
+const signinUri = '/api/users/signin';
+const signupUri = '/api/users/signup';
+
+it('fails when a email that does not exist is supplied', async () => {
+  await request(app)
+    .post(signinUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post(signinUri)
+    .send({
+      email: 'test@test.com',
+      password: 'aslkdfjalskdfj',
+    })
+    .expect(400);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const currentuser = await request(app).get('api/users/currentuser');
+  console.log('currentuser', currentuser.status);
+
+  const response = await request(app).post(signinUri).send({
+    email: 'test@test.com',
+    password: 'password',
+  });
+  // .expect(200);
+
+  // console.log('response status', response.status);
+  // console.log('response header', response.header);
+  // expect(response.get('Set-Cookie')).toBeDefined();
+});
--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -4,69 +4,79 @@ import {app} from '../../app';
 
 const signupUri = '/api/users/signup';
 
-describe('Routes: signup', () => {
-  it('returns a 201 on successful signup ', async () => {
-    return request(app)
-      .post(signupUri)
-      .send({
-        email: 'test@test.com',
-        password: 'password',
-      })
-      .expect(201);
-  });
+it('returns a 201 on successful signup ', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
 
-  it('returns a 400 on invalid email', async () => {
-    return request(app)
-      .post(signupUri)
-      .send({
-        email: 'badEmail',
-        password: 'password',
-      })
-      .expect(400);
-  });
+it('returns a 400 on invalid email', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'badEmail',
+      password: 'password',
+    })
+    .expect(400);
+});
 
-  it('returns a 400 on invalid password', async () => {
-    return request(app)
-      .post(signupUri)
-      .send({
-        email: 'test@test.com',
-        password: 'x',
-      })
-      .expect(400);
-  });
+it('returns a 400 on invalid password', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'x',
+    })
+    .expect(400);
+});
 
-  it('returns a 400 with missing email and/or password ', async () => {
-    await request(app)
-      .post(signupUri)
-      .send({
-        email: '',
-        password: 'password',
-      })
-      .expect(400);
-    await request(app)
-      .post(signupUri)
-      .send({
-        email: 'test@test.com',
-        password: '',
-      })
-      .expect(400);
-    await request(app).post(signupUri).send({}).expect(400);
-  });
+it('returns a 400 with missing email and/or password ', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: '',
+      password: 'password',
+    })
+    .expect(400);
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: '',
+    })
+    .expect(400);
+  await request(app).post(signupUri).send({}).expect(400);
+});
 
-  it('should not allow duplicate emails', async () => {
-    await request(app)
-      .post(signupUri)
-      .send({
-        email: 'test@test.com',
-        password: 'password',
-      })
-      .expect(201);
-    await request(app)
-      .post(signupUri)
-      .send({
-        email: 'test@test.com',
-        password: 'password',
-      })
-      .expect(400);
-  });
+it('should not allow duplicate emails', async () => {
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+  await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('should set a cookie after a successful sign in', async () => {
+  const response = await request(app)
+    .post(signupUri)
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
 });

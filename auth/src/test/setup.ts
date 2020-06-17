86af5ad6fbcca1846052e851b207063965419f7a
nickcoleman <ncoleman@marketaxess.com>
2020-06-17 08:17:17 -0600

    test environment setup with jest and in-memory mongodb

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import mongoose, {mongo} from 'mongoose';
+
+import {app} from '../app';
+
+let mongo: any;
+beforeAll(async () => {
+  mongo = new MongoMemoryServer(); // create a new instance of mongodb running in memory
+  const mongoUri = await mongo.getUri(); // get URI for mongodb running in memory
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// delete any existing colletions before each test
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// remove the running instances of mongodb and mongoose collections
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});

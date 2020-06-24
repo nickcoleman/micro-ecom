55f922c285be0041d0015a4d7e6733a4a0271fe7
nickcoleman <nick@nickcoleman.io>
2020-06-24 15:24:54 -0600

    use @eticket/common package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
+import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
+import {errorHandler, NotFoundError} from '@eticket/common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signupRouter} from './routes/signup';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,7 +15,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
 
@@ -31,4 +30,4 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-export { app };
+export {app};
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,30 +1,27 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
+import {validateRequest, BadRequestError} from '@eticket/common';
 
-import { Password } from '../services/password';
-import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import {Password} from '../services/password';
+import {User} from '../models/user';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signin',
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
+    body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
       .notEmpty()
-      .withMessage('You must supply a password')
+      .withMessage('You must supply a password'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({email});
     if (!existingUser) {
       throw new BadRequestError('Invalid credentials');
     }
@@ -41,18 +38,18 @@ router.post(
     const userJwt = jwt.sign(
       {
         id: existingUser.id,
-        email: existingUser.email
+        email: existingUser.email,
       },
       process.env.JWT_KEY!
     );
 
     // Store it on session object
     req.session = {
-      jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(200).send(existingUser);
   }
 );
 
-export { router as signinRouter };
+export {router as signinRouter};
